42f9983ebdc3f54f57b9acbbcb952132d4365ade
darajsv <darajsv@gmail.com>
2023-07-23 20:36:22 -0300

    adds cors

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ import { detailLocation } from './src/services/detailLocation.js';
 import { detailPlan } from './src/services/detailPlan.js';
 import { getRoutes } from './src/services/getRoutes.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
+
 const app = express();
 const port = 3000;
 
@@ -14,8 +16,9 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: '*', // Replace with your Vue.js frontend domain
+    origin: 'http://ec2-34-238-39-60.compute-1.amazonaws.com:8080', // Replace with your Vue.js frontend domain
     methods: 'GET, POST, PUT, DELETE', // Add the HTTP methods your frontend will use
+    credentials: true, // Set to true if your frontend includes credentials in the request (e.g., cookies)
   })
 );
 
