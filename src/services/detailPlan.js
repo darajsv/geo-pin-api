b591898c94fe1d1da9991a71e751bc479e8dcaa6
darajsv <darajsv@gmail.com>
2023-07-23 19:29:37 -0300

    debug

--- src/services/detailPlan.js
+++ src/services/detailPlan.js
@@ -1,7 +1,8 @@
 import plansRepository from '../repositories/plans.repository.js';
 
 export const detailPlan = async (planId) => {
+  console.log(planId);
   const plan = await plansRepository.findByCodeWithPins(planId);
-
+  console.log('passei ', plan);
   return { statusCode: 200, body: JSON.stringify(plan, null, 2) };
 };
