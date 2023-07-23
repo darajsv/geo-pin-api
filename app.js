3528f6bb4a50dbd627f6a4870993b9763cab6c47
darajsv <darajsv@gmail.com>
2023-07-23 20:36:35 -0300

    adds cors

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ app.use(
   cors({
     origin: 'http://ec2-34-238-39-60.compute-1.amazonaws.com:8080', // Replace with your Vue.js frontend domain
     methods: 'GET, POST, PUT, DELETE', // Add the HTTP methods your frontend will use
-    credentials: true, // Set to true if your frontend includes credentials in the request (e.g., cookies)
   })
 );
 
